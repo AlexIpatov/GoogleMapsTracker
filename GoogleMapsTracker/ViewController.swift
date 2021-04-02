d0ae54f4d4f4870b453072a3103c1b5d31f65d40
AlexIpatov <65159241+AlexIpatov@users.noreply.github.com>
2021-04-02 17:17:47 +0300

    init

---
+++ GoogleMapsTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoogleMapsTracker
+//
+//  Created by Александр Ипатов on 02.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
